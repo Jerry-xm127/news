e633aee504b6076ee7eb6a66a8bc9cff82789736
Jerry <77833408@qq.com>
2018-03-06 18:58:46 +0800

    第一次

---
+++ src/com/news/bean/News.java
@@ -0,0 +1,10 @@
+package com.news.bean;
+
+import java.io.Serializable;
+
+public class News implements Serializable{
+	
+	private static final long serialVersionUID = -2726929297126310844L;
+	
+	private String name;
+}
