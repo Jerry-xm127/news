d7110e3e4806871c051dd0e0f289098f08a28209
Jerry <77833408@qq.com>
2018-03-07 23:23:54 +0800

    没完全实现增量修改

--- src/com/news/dao/impl/NewsDaoImpl.java
+++ src/com/news/dao/impl/NewsDaoImpl.java
@@ -37,14 +37,111 @@ public class NewsDaoImpl implements INewsDao{
 
 	@Override
 	public void del(Integer id, Connection conn) throws Exception {
-		// TODO Auto-generated method stub
-		
+		if(conn == null) {
+			throw new Exception("数据库连接加载失败");
+		}
+		if(id == null) {
+			throw new Exception("id为空");
+		}
+		String sql = "DELETE FROM news  WHERE news_id=?;";
+		PreparedStatement pst = conn.prepareStatement(sql);
+		pst.setInt(1, id);
+		pst.executeUpdate();
+		pst.close();
 	}
 
 	@Override
 	public void update(News news, Connection conn) throws Exception {
-		// TODO Auto-generated method stub
+		if(conn == null) {
+			throw new Exception("数据库连接加载失败");
+		}
+		PreparedStatement pst = null;
+		StringBuilder sb = new StringBuilder("UPDATE news n SET ");
+		int index = 0;
+		int indexNewsTitle = 0;
+		int indexNewsIntro = 0;
+		int indexNewsContent = 0;
+		int indexNewsAuthor = 0;
+		int indexNewsColumn = 0;
+		int indexNewsDate = 0;
+		int indexBrowseCount = 0;
+		int indexKeywords = 0;
+		if(news != null) {
+			if(news.getNewsTitle() != null) {
+				sb.append("n.news_title=?");
+				indexNewsTitle = ++index;
+			}
+			if(news.getNewsIntro() != null) {
+				sb.append(",n.news_intro=?");
+				indexNewsIntro = ++index;
+			}
+			if(news.getNewsContent() != null) {
+				sb.append(",n.news_content=?");
+				indexNewsContent = ++index;
+			}
+			if(news.getNewsIntro() != null) {
+				sb.append(",n.news_author=?");
+				indexNewsAuthor = ++index;
+			}
+			if(news.getColumn() != null) {
+				if(news.getColumn().getColumnId() != null) {
+					sb.append(",n.columns_id=?");
+					indexNewsColumn = ++index;
+				}
+			}
+			if(news.getNewsDate() != null) {
+				sb.append(",n.news_date=?");
+				indexNewsDate = ++index;
+			}
+			if(news.getBrowseCount() != null) {
+				sb.append(",n.browse_count=?");
+				indexBrowseCount = ++index;
+			}
+			if(news.getKeywords() != null) {
+				if(news.getKeywords().getKeyId() != null) {
+					sb.append(",n.key_id=? ");
+					indexKeywords = ++index;
+				}
+			}
+			sb.append(" WHERE n.news_id=?");
+		}
 		
+		pst = conn.prepareStatement(sb.toString());
+		
+		if(news != null) {
+			if(news.getNewsTitle() != null) {
+				pst.setString(indexNewsTitle, news.getNewsTitle());
+			}
+			if(news.getNewsIntro() != null) {
+				pst.setString(indexNewsIntro, news.getNewsIntro());
+			}
+			if(news.getNewsContent() != null) {
+				pst.setString(indexNewsContent, news.getNewsContent());
+			}
+			if(news.getNewsIntro() != null) {
+				pst.setString(indexNewsAuthor, news.getNewsAuthor());
+			}
+			if(news.getColumn() != null) {
+				if(news.getColumn().getColumnId() != null) {
+					pst.setInt(indexNewsColumn, news.getColumn().getColumnId());
+				}
+			}
+			if(news.getNewsDate() != null) {
+				pst.setDate(indexNewsDate, DateUtil.utilChangeSql(news.getNewsDate()));
+			}
+			if(news.getBrowseCount() != null) {
+				pst.setInt(indexBrowseCount, news.getBrowseCount());
+			}
+			if(news.getKeywords() != null) {
+				if(news.getKeywords().getKeyId() != null) {
+					pst.setInt(indexKeywords, news.getKeywords().getKeyId());
+				}
+			}
+				pst.setInt(index + 1, news.getNewsId());
+		}
+		
+		pst.executeUpdate();
+		pst.close();
 	}
 
 	@Override
--- src/com/news/test/TestNews.java
+++ src/com/news/test/TestNews.java
@@ -14,7 +14,7 @@ import com.news.util.C3p0Util;
 
 class TestNews {
 
-	@Test
+	//@Test
 	void testAdd() {
 		try {
 			Connection conn = C3p0Util.getConn();
@@ -38,5 +38,44 @@ class TestNews {
 			e.printStackTrace();
 		}
 	}
+	
+	//@Test
+	void testDel() {
+		try {
+			Connection conn = C3p0Util.getConn();
+			INewsDao dao = new NewsDaoImpl();
+			Integer id = 3;
+			dao.del(id, conn);
+			C3p0Util.closeConn(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+	
+	//增量修改有点小问题
+	@Test
+	void testUpdate() {
+		try {
+			Connection conn = C3p0Util.getConn();
+			News news = new News();
+			Column column = new Column();
+			Keywords keywords = new Keywords();
+			news.setNewsTitle("ss");
+			news.setNewsIntro("ss");
+			news.setNewsContent("ss");
+			news.setNewsAuthor("ss");
+			news.setNewsDate(new Date());
+			column.setColumnId(2);
+			news.setColumn(column);
+			news.setBrowseCount(55);
+			news.setNewsId(5);
+			INewsDao dao = new NewsDaoImpl();
+			dao.update(news, conn);
+			C3p0Util.closeConn(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
